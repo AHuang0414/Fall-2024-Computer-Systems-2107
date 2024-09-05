517e4a0e281c488904dd3f1abcf54c11cd3a0724
FakeFoody <148585080+AHuang0414@users.noreply.github.com>
2024-09-05 00:55:49 -0400

    started paycheck

---
+++ paycheck.c
@@ -0,0 +1,17 @@
+/*
+Andrew Huang
+
+9/5/2024
+
+2107
+
+Lab 2 Paycheck
+
+Write a C program called paycheck to calculate the paycheck for a Temple employee based on the hourlySalary, weeklyTime
+(working for maximum 40 hours) and overtime (working for more than 40 hours).
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
