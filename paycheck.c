a2a24ceb71731db95132c5d26c74f8635875af11
AHuang0414 <148585080+AHuang0414@users.noreply.github.com>
2024-09-06 16:07:35 -0400

    paycheck finished

--- paycheck.c
+++ paycheck.c
@@ -18,25 +18,31 @@ Write a C program called paycheck to calculate the paycheck for a Temple employe
 
 
 int main() {
-    int ID;
+    double ID;
     float hourlySalary, weeklyHours, overtimeHours = 0;
-    
+    double intPart, fracPart;
+
     printf("Welcome to \"TEMPLE HUMAN RESOURCES\"\n");
     
     
     printf("\n\tEnter Employee Number: "); // getting employee ID
-    scanf("%d", &ID);
-    if (ID <= 0) {
-        printf("\tThis is not a valid Employee Number. \n\tPlease run the program again");
-        return 0;
+    scanf("%lf", &ID);
+    fracPart = modf(ID, &intPart);
+    // printf("FracPart: %lf", fracPart);
+    // printf("IntPart: %lf", intPart);
+    if (ID <= 0 || fracPart != 0) {
+        printf("\n\tThis is not a valid Employee Number. \n\tPlease run the program again\n");
+        printf("\nThank you for using \"TEMPLE HUMAN RESOURCES\"\n");
+        exit(1);
     } 
     
 
     printf("\tEnter Hourly Salary: "); // getting hourly salary
     scanf("%f", &hourlySalary);
     if (hourlySalary <= 0) {
-        printf("\tThis is not a valid hourly salary amount. \n\tPlease run the program again");
-        return 0;
+        printf("\n\tThis is not a valid hourly salary amount. \n\tPlease run the program again\n");
+        printf("\nThank you for using \"TEMPLE HUMAN RESOURCES\"\n");
+        exit(1);
     } 
     
 
@@ -44,8 +50,9 @@ int main() {
     scanf("%f", &weeklyHours);
     if (weeklyHours <= 0) 
     {
-        printf("\tThis is not a weekly time. \n\tPlease run the program again");
-        return 0;
+        printf("\n\tThis is not a weekly time. \n\tPlease run the program again\n");
+        printf("\nThank you for using \"TEMPLE HUMAN RESOURCES\"\n");
+        exit(1);
     } 
     else if (weeklyHours > 40)
     {
@@ -62,18 +69,18 @@ int main() {
 
     printf("\t=================================================\n");
 
-    printf("\tEmployee #: %d\n", ID);
+    printf("\tEmployee #: %.f\n", ID);
     printf("\tHourly Salary: $%.2f\n", hourlySalary);
     printf("\tWeekly Time: %.2f\n", weeklyHours);
     printf("\tRegular Pay: $%.2f\n", regularPay);
     printf("\tOvertime Pay: $%.2f\n", overtimePay);
     printf("\tNet Pay: $%.2f", netPay);
     
-    printf("\n\t=================================================");
+    printf("\n\t=================================================\n");
 
-    printf("\nThank you for using \"TEMPLE HUMAN RESOURCES\"");
+    printf("\nThank you for using \"TEMPLE HUMAN RESOURCES\"\n");
 
-    return 0;
+    exit(0);
 }
 
 
