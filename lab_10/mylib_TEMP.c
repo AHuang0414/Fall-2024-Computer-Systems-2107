8a01bc7ea23b98bd7e39456144c64182f6ae8d5f
FakeFoody <148585080+AHuang0414@users.noreply.github.com>
2024-11-23 17:09:52 -0500

    lab 10 WIP

--- lab_10/mylib_TEMP.c
+++ lab_10/mylib_TEMP.c
@@ -28,3 +28,125 @@ int num_in_range(char *s1, char b, char t) {
 
     return num;
 }
+
+int diff(char *s1, char *s2) {
+    int diffCount = 0;
+
+    int length1 = 0;
+    while(s1[length1]!='\0')
+    {
+        length1++;
+        // printf("Length 1: %i\n", length1);
+    }
+
+    int length2 = 0;
+    while(s2[length2]!='\0')
+    {
+        length2++;
+        // printf("Length 2: %i\n", length2);
+    }
+
+    if (length1 > length2) {
+        // puts("s1 larger");
+        length2 = length1;
+        length1 = 0;
+        while (length1 < length2) {
+            if (s1[length1] != s2[length1]) diffCount++;
+            length1++;
+        }
+    }
+    else if (length1 == length2) {
+        // puts("equal length");
+        length1 = 0;
+        while (length1 < length2) {
+            if (s1[length1] != s2[length1]) diffCount++;
+            length1++;
+        }
+    }
+    else {
+        // puts("s2 larger");
+        length1 = length2;
+        length2 = 0;
+        while (length1 > length2) {
+            if (s1[length2] != s2[length2]) diffCount++;
+            length2++;
+        }
+    } 
+    
+    return diffCount;
+}
+
+void shorten(char *s, int new_len) {
+    // printf("%d\n", sizeof(char));
+    char *newStr = (char *)calloc(new_len+1, sizeof(char)); // +1 for '\0'
+    if (newStr == NULL) {
+        printf("Memory allocation failed.\n");
+        return;
+    }
+    
+    int index = 0;
+    while (index < new_len && s[index] != '\0' )
+    {
+        newStr[index] = s[index];
+        index++;
+    }
+    
+    newStr[new_len] = '\0';
+
+    printf("%s", newStr);
+    free(newStr);
+}
+
+int len_diff(char *s1, char *s2) {
+    int length1 = 0;
+    int length2 = 0;
+
+    while(s1[length1]!='\0')
+    {
+        length1++;
+    }
+    while(s2[length2]!='\0')
+    {
+        length2++;
+    }
+
+    return length1 - length2;
+}
+
+void rm_left_space(char *s) {
+    int startIndex = 0;
+    int length = 0;
+    while(s[length]!='\0')
+    {
+        if (!isspace(s[length])) {
+            startIndex = length;
+            break;
+        }
+        length++;
+    }
+    
+    printf("%s", (s + startIndex));
+}
+
+void rm_right_space(char *s) {
+    int length = 0;
+    while (s[length] != '\0') {
+        // printf("%c ", s[length]);
+        length++;
+    }
+    // printf("%d", length);
+
+    while (length > 0) {
+        if (!isspace(s[length-1])) {
+            // printf("%d", length);
+            break;
+        }
+        length--;
+    }
+    
+    printf("%.*s", length, s);
+}
+
+void rm_space(char *s) {
+    
+}
--- lab_10/strtester.c
+++ lab_10/strtester.c
@@ -8,15 +8,88 @@ int main() {
     printf("all_letters(\"Hello World\"): %d\n", all_letters("Hello World")); //  1
     printf("all_letters(\"He11o World\"): %d\n", all_letters("He11o World")); //  0
 
+    puts("");
+
     // Testing num_in_range
     printf("num_in_range(\"abcdef\", 'b', 'e'): %d\n", num_in_range("abcdef", 'b', 'e')); //  4
     printf("num_in_range(\"Yellow\", 'f', 'm'): %d\n", num_in_range("Yellow", 'f', 'm')); //  2
 
+    puts("");
+
     // Testing diff
     printf("diff(\"hello\", \"hello\"): %d\n", diff("hello", "hello"));  //  0
     printf("diff(\"hello\", \"hella\"): %d\n", diff("hello", "hella"));  //  1
-    printf("diff(\"hello\", \"hellooo\"): %d\n", diff("hello", "hellooo"));  //  2
+    printf("diff(\"\", \"abcd\"): %d\n", diff("", "abcd"));  // Expected: 4
+    printf("diff(\"abc\", \"\"): %d\n", diff("abc", ""));  // Expected: 3
+    printf("diff(\"Book\", \"Back\"): %d\n", diff("Book", "Back"));  //  2
 
+    puts("");
+    
+    // Testing shorten
+    printf("shorten(\"Programming\", 7): ");
+    shorten("Programming", 7); // Expected: "Program"
+    puts("");
+
+    printf("shorten(\"Data Structures\", 20): ");
+    shorten("Data Structures", 20); // Expected: "Data Structures"
+    puts("");
+
+    printf("shorten(\"Hello World\", 5): ");
+    shorten("Hello World", 5); // Expected: "Hello"
+    puts("");
+
+    printf("shorten(\"Hello World\", 20): ");
+    shorten("Hello World", 20); // Expected: "Hello World"
+    puts("");
+
+    puts("");
+
+    // Testing len_diff
+    printf("len_diff(\"Philadelphia\", \"Hello\"): %d\n", len_diff("Philadelphia", "Hello")); // Expected: 7
+    printf("len_diff(\"Program\", \"Code\"): %d\n", len_diff("Program", "Code")); // Expected: 3
+
+    puts("");
+
+    // Testing rm_left_space
+    printf("rm_left_space(\"   Leading spaces\"): ");
+    rm_left_space("   Leading spaces"); // Expected: "Leading spaces"
+    puts("");
+
+    printf("rm_left_space(\"No Leading Spaces\"): ");
+    rm_left_space("No Leading Spaces"); // Expected: "No Leading Spaces"
+    puts("");
+
+    printf("rm_left_space(\"     Hello World\"): ");
+    rm_left_space("     Hello World"); // Expected: "Hello World"
+    puts("");
+
+    printf("rm_left_space(\"     Hello\"): ");
+    rm_left_space("     Hello"); // Expected: "Hello"
+    puts("");
+
+    puts("");
+
+    // Testing rm_right_space
+    printf("rm_right_space(\"Hello \"): ");
+    rm_right_space("Hello "); // Expected: "Hello"
+    puts("");
+
+    printf("rm_right_space(\"Hello World   \"): ");
+    rm_right_space("Hello World   "); // Expected: "Hello World"
+    puts("");
+
+    puts("");
+
+    // Testing rm_space
+    printf("rm_space(\" Hello \"): ");
+    rm_space(" Hello "); // Expected: "Hello"
+    puts("");
+
+    printf("rm_space(\"  Trimmed  \"): ");
+    rm_space("  Trimmed  "); // Expected: "Trimmed"
+    puts("");
+
+    puts("");
 
     return 0;
 }
