043483638192b8a6c1a7bf1a7c8d98ca93761be6
FakeFoody <148585080+AHuang0414@users.noreply.github.com>
2024-11-25 20:15:20 -0500

    lab 10 wip

--- lab_10/mylib_TEMP.c
+++ lab_10/mylib_TEMP.c
@@ -1,7 +1,7 @@
 #include "mylib.h"
-#include <stdio.h>
-#include <memory.h>
 #include <ctype.h>
+#include <memory.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h> 
 
@@ -125,7 +125,7 @@ void rm_left_space(char *s) {
         length++;
     }
     
-    printf("%s", (s + startIndex));
+    printf("\"%s\"", (s + startIndex));
 }
 
 void rm_right_space(char *s) {
@@ -144,9 +144,254 @@ void rm_right_space(char *s) {
         length--;
     }
     
-    printf("%.*s", length, s);
+    printf("\"%.*s\"", length, s);
 }
 
 void rm_space(char *s) {
+    int length = 0;
+    while (s[length] != '\0') {
+        // printf("%c ", s[length]);
+        length++;
+    }
+
+    char newStr[length+1];
+
+    int index = 0;
+    while (*s != '\0') {
+        if (!isspace(*s)) {
+            newStr[index] = *s;
+            index++;
+        }
+        s++;
+    }
+
+    newStr[index] = '\0';
+
+    printf("\"%s\"", newStr);
+}
+
+int find(char *h, char *n) {
+    for (int i = 0; h[i] != '\0'; i++) {
+        if (h[i] == *n) return i;
+    }
+
+    return -1;
+}
+
+char *ptr_to(char *h, char *n) {
+    while (*h != '\0') {
+        if (*n == *h) {
+            return h;
+        }
+        h++;
+    }
+
+    return NULL;
+}
+
+int is_empty(char *s) {
+    if (s == NULL || s[0] == '\0') return 1;
+
+    while (*s != '\0') {
+        if (!isspace(*s)) {
+            return 0;
+        }
+        s++;
+    }
+
+    return 1; //assume empty by default
+}
+
+char *str_zip(char *s1, char *s2) {
+    int LENGTH_PART_1 = 0;
+    int LENGTH_PART_2 = 0;
+    while (s1[LENGTH_PART_1] != '\0') {
+        LENGTH_PART_1++;
+    }
+    while (s2[LENGTH_PART_2] != '\0') {
+        LENGTH_PART_2++;
+    }
+    int LENGTH = LENGTH_PART_1 + LENGTH_PART_2; 
+    // printf("%d\n", LENGTH);
+
+    char *combinedString = (char *)malloc((LENGTH+1) * sizeof(char)); // +1 for '\0'
+    combinedString[0] = 0;
+
+    int index = 0, index_1 = 0, index_2 = 0;
+    while(index < LENGTH) {
+        if (index_1 < LENGTH_PART_1) {
+            combinedString[index] = s1[index_1];
+            index_1++;
+            index++;
+        }
+        if (index_2 < LENGTH_PART_2) {
+            combinedString[index] = s2[index_2];
+            index_2++;
+            index++;
+        }
+    }
+    combinedString[LENGTH] = '\0';
+
+    return combinedString;
+}
+
+void capitalize(char *s) { 
+    int LENGTH = 0;
+    while (s[LENGTH] != '\0') LENGTH++;
+
+    char *newStr = (char*)malloc(sizeof(char) * LENGTH+1); // +1 for '\0'
+    
+    int index = 0;
+    while (s[index] != '\0') {
+        if (isalpha(s[index]) && (index == 0 || isspace(s[index-1]))) {
+            newStr[index] = toupper(s[index]);
+        }
+        else {
+            newStr[index] = tolower(s[index]);
+        }
+        index++;
+    }
+
+    newStr[LENGTH] = '\0';
+
+    printf("%s", newStr);
+    free(newStr);
+}
+
+int strcmp_ign_case(char *s1, char *s2) {
+    // A < Z (A is valued less than Z b/c ASCII)
+    // A appears before Z
+    //CASE 1: if s1, then s2 (-)
+    //CASE 2: if s2, then s1 (+)
+    //DEFAULT: equal (0)
+
+    while (*s1 != '\0' || *s2 != '\0') {
+        if (toupper(*s1) < toupper(*s2)) { // CASE 1
+            return -1;
+        }
+        else if (toupper(*s1) == toupper(*s2)) {
+            s1++;
+            s2++;
+            continue;
+        }
+        else {
+            return 1;
+        }
+    }
+
+    return 0; //assume equal default
+}
+
+void take_last(char *s, int n) {
+    int LENGTH = 0;
+    while (s[LENGTH] != '\0') LENGTH++;
+
+    if (n >= LENGTH) return;
+    
+    for (int index = 0; s[LENGTH-n + index] != '\0'; index++) {
+        s[index] = s[LENGTH-n + index];
+    }
+
+    s[n] = '\0';
+}
+
+char *dedup(char *s) {
+    int LENGTH = 0;
+    while (s[LENGTH] != '\0') LENGTH++;
+    char *newStr = (char*)(malloc(sizeof(char) * LENGTH+1));
+
+    int index = 0, newIndex = 0;
+    while (s[index] != '\0') {
+        bool isDupe = false; 
+
+        // Check if the current character exists in newStr
+        for (int i = 0; i < newIndex; i++) {
+            if (newStr[i] == s[index]) {
+                isDupe = true;
+                break;
+            }
+        }
+        if (!isDupe) {
+            newStr[newIndex] = s[index];
+            newIndex++;
+        }
+        index++;
+    }
+    newStr[newIndex] = '\0';
+
+    return newStr;
+}
+
+char *pad(char *s, int d) {
+    int LENGTH = 0;
+    while (s[LENGTH] != '\0') LENGTH++;
+
+    if (d < LENGTH) d = LENGTH;
+
+    char *newStr = (char*)malloc(sizeof(char) * d+1);
+    if (newStr == NULL) return NULL;
+
+    int index = 0;
+    while (index < d) {
+        if (*(s + index) != '\0' && index < LENGTH) {
+            *(newStr + index) = *(s + index);
+        }
+        else {
+            *(newStr + index) = ' ';
+        }
+        index++;
+    }
+    *(newStr + d) = '\0';
+
+    return newStr;
+}
+
+int ends_with_ignore_case(char *s, char *suff) {
+    int SUFF_LENGTH = 0, LENGTH = 0;
+    while (suff[SUFF_LENGTH] != '\0') SUFF_LENGTH++;
+    while (s[LENGTH] != '\0') LENGTH++;
+    
+    int startIndex = LENGTH - SUFF_LENGTH;
+    if (startIndex < 0) return 0;
+
+    int count = 0;
+    while (s[startIndex]!= '\0') {
+        if (toupper(suff[count]) == toupper(s[startIndex])) count++;
+        startIndex++;
+    }
+
+    if (count != SUFF_LENGTH) return 0;
+
+    return 1; //assume it is suffix
+}
+
+char *repeat(char *s, int x, char sep) {
+    if (s == NULL) return NULL;
+
+    int LENGTH = 0;
+    while (s[LENGTH] != '\0') LENGTH++;
+    int TOTAL_LENGTH = (LENGTH * x) + (x-1);
+
+    char *newStr = (char*)malloc(sizeof(char) * (TOTAL_LENGTH + 1));
+    if (newStr == NULL) return NULL;  
+
+    int total_index = 0;
+    for (int i = 0; i < x; i++) {
+        int index = 0;
+        while (s[index] != '\0') {
+            newStr[total_index] = s[index];
+            total_index++;
+            index++;
+        }
+        newStr[total_index] = sep;
+        total_index++;
+    }
+    newStr[TOTAL_LENGTH] = '\0';
+
+    return newStr;
+}
+
+char *replace(char *s, char *pat, char *rep) {
     
 }
+
--- lab_10/strtester.c
+++ lab_10/strtester.c
@@ -1,7 +1,6 @@
+#include "mylib.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "mylib.h"
 
 int main() {
     // Testing all_letters
@@ -90,6 +89,136 @@ int main() {
     puts("");
 
     puts("");
+
+    // Testing find
+    printf("find(\"Hello\", \"l\"): %d\n", find("Hello", "l")); // Expected: 2
+    printf("find(\"Hello\", \"q\"): %d\n", find("Hello", "q")); // Expected: -1
+    printf("find(\"Mississippi\", \"iss\"): %d\n", find("Mississippi", "iss")); // Expected: 1
+
+    puts("");
+
+    // Testing ptr_to
+    char *result = ptr_to("Hello", "l");
+    printf("ptr_to(\"Hello\", \"l\"): %s\n", result ? result : "NULL"); // Expected: "llo"
+    
+    result = ptr_to("Hello", "q");
+    printf("ptr_to(\"Hello\", \"q\"): %s\n", result ? result : "NULL"); // Expected: NULL
+
+    puts("");
+
+    // Testing is_empty
+    printf("is_empty(\" \"): %d\n", is_empty(" "));  // Expected: 1 (Only whitespace)
+    printf("is_empty(\"Hello\"): %d\n", is_empty("Hello"));  // Expected: 0 (Non-empty string)
+    printf("is_empty(NULL): %d\n", is_empty(NULL));  // Expected: 1 (NULL string)
+
+    puts("");
+
+    // Testing str_zip
+    printf("str_zip(\"Temple\", \"Hello\"): %s\n", str_zip("Temple", "Hello")); // Expected: "THeemlplloe"
+    printf("str_zip(\"Spongebob\", \"Patrick\"): %s\n", str_zip("Spongebob", "Patrick")); // Expected: "SPpaotnrgiecbkob"
+    printf("str_zip(\"abc\", \"123\"): %s\n", str_zip("abc", "123")); // Expected: "a1b2c3"
+
+    puts("");
+
+    // Testing capitalize
+    printf("capitalize(\"hello world\"): ");
+    capitalize("hello world"); // Expected: "Hello World"
+    puts("");
+
+    printf("capitalize(\"sky\"): ");
+    capitalize("sky"); // Expected: "Sky"
+    puts(""); 
+
+    printf("capitalize(\"gOoD mOrNiNg tO yOu\"): ");
+    capitalize("gOoD mOrNiNg tO yOu"); // Expected: "Good Morning To You"
+    puts(""); 
+
+    printf("capitalize(\"mY nAmE\"): ");
+    capitalize("mY nAmE"); // Expected: "My Name"
+    puts(""); 
+
+    puts(""); 
+
+    // Testing strcmp_ign_case
+    printf("strcmp_ign_case(\"hello\", \"goodbye\"): %d\n", strcmp_ign_case("hello", "goodbye")); // Expected: positive (e.g., 1)
+    printf("strcmp_ign_case(\"Hello\", \"hello\"): %d\n", strcmp_ign_case("Hello", "hello"));     // Expected: 0
+    printf("strcmp_ign_case(\"apple\", \"zebra\"): %d\n", strcmp_ign_case("apple", "zebra"));     // Expected: negative (e.g., -1)
+
+    puts("");
+
+    // Testing take_last
+    char test1[] = "hello"; 
+    char test2[] = "hello";
+    char test3[] = "world";
+    char test4[] = "Brubeck";
+    take_last(test1, 3);
+    printf("take_last(\"hello\", 3): %s\n", test1); // Expected: "llo"
+    take_last(test2, 6);
+    printf("take_last(\"hello\", 6): %s\n", test2); // Expected: "hello"
+    take_last(test3, 2);
+    printf("take_last(\"world\", 2): %s\n", test3); // Expected: "ld"
+    take_last(test4, 5);
+    printf("take_last(\"Brubeck\", 5): %s\n", test4); // Expected: "ubeck"
+
+    puts("");
+
+    // Testing dedup
+    char *ded1 = dedup("hello");
+    printf("dedup(\"hello\"): %s\n", ded1); // Expected: "helo"
+    free(ded1);
+
+    char *ded2 = dedup("There's always money in the banana stand.");
+    printf("dedup(\"There's always money in the banana stand.\"): %s\n", ded2); // Expected: "Ther's alwymonitbd."
+    free(ded2);
+
+    char *ded3 = dedup("aabbccdd");
+    printf("dedup(\"aabbccdd\"): %s\n", ded3); // Expected: "abcd"
+    free(ded3);
+
+    puts("");
+
+    // Testing pad
+    char *padded1 = pad("hello", 6);
+    printf("pad(\"hello\", 6): \"%s\"\n", padded1); // Expected: "hello "
+    free(padded1);
+
+    char *padded2 = pad("hello", 5);
+    printf("pad(\"hello\", 5): \"%s\"\n", padded2); // Expected: "hello"
+    free(padded2);
+
+    char *padded3 = pad("world", 10);
+    printf("pad(\"world\", 10): \"%s\"\n", padded3); // Expected: "world     "
+    free(padded3);
+
+    char *padded4 = pad("pad", 2);
+    printf("pad(\"pad\", 2): \"%s\"\n", padded4); // Expected: "pad"
+    free(padded4);
+
+    puts("");
+
+    // Testing ends_with_ignore_case
+    printf("ends_with_ignore_case(\"Coding\", \"ing\"): %d\n", ends_with_ignore_case("Coding", "ing")); // Expected: 1
+    printf("ends_with_ignore_case(\"Coding\", \"ed\"): %d\n", ends_with_ignore_case("Coding", "ed"));   // Expected: 0
+    printf("ends_with_ignore_case(\"ELON MUSK\", \"sk\"): %d\n", ends_with_ignore_case("ELON MUSK", "sk")); // Expected: 1
+
+    puts("");
+
+    // Testing repeat
+    char *repeat1 = repeat("hello", 3, '-');
+    printf("repeat(\"hello\", 3, '-'): %s\n", repeat1);  // Expected: "hello-hello-hello"
+    free(repeat1);
+
+    char *repeat2 = repeat("all right", 2, ',');
+    printf("repeat(\"all right\", 2, ','): %s\n", repeat2);  // Expected: "all right,all right"
+    free(repeat2);
+
+    char *repeat3 = repeat("test", 4, ' ');
+    printf("repeat(\"test\", 4, ' '): %s\n", repeat3);  // Expected: "test test test test"
+    free(repeat3);
+
+    puts("");
+
+    
 
     return 0;
 }
